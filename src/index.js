779a6c987ca53a5e88d9d8aee275ea02f59299c7
Charles Kihe <charles@simplesessions.com>
2018-04-02 21:19:16 -1000

    Starter project that has a basic react/bable/webpack setup, as well as styled components and storybook integrated

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'WealthFit Design System'
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+)
+
+module.hot.accept()
---
+++ src/stories/index.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Welcome } from '@storybook/react/demo';
+
+import Button from '../components/Button'
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('primary with text', () => <Button primary onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
